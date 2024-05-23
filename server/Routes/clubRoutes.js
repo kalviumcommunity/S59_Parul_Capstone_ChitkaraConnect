1f80b9a12f3eea78b3a02f3e5cf2f61b3013d4b7
Parul Chauhan <parulchauhan1357@gmail.com>
2024-05-23 15:42:42 +0530

    Added semicolon to new POST route

--- server/Routes/clubRoutes.js
+++ server/Routes/clubRoutes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { getClubData, getOneClubData, postClubData } = require('../Controller/clubController')
 
-router.get('/clubs', getClubData)
-router.get('/club/:clubID', getOneClubData)
-router.post('/clubs', postClubData)
+router.get('/clubs', getClubData);
+router.get('/club/:clubID', getOneClubData);
+router.post('/clubs', postClubData);
 
 module.exports = router 
