fa8fc289355e7fe068b513d3d31bcbed72921618
Parul Chauhan <parulchauhan1357@gmail.com>
2024-05-23 18:00:17 +0530

    Made carousel for clubs page

---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import carouselOne from "../assets/ChitkUni.png"
+import carouselTwo from "../assets/ChitkUni.png"
+import carouselThree from "../assets/ChitkUni.png"
+
+
+export default function SimpleSlider() {
+    var settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 3000,
+        autoplaySpeed: 5000,
+        cssEase: "linear",
+    };
+
+    return (
+        <div style={{ margin: "auto", width: "40vw", marginTop : "50px" }}>
+            <Slider {...settings}>
+                <img src={carouselOne} alt="club" className="rounded"/>
+                <img src={carouselTwo} alt="club" className="rounded"/>
+                <img src={carouselThree} alt="club" className="rounded"/>
+            </Slider>
+        </div>)
+}
